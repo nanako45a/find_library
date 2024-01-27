a3e24b87fd09030003b5feed41f77a9882991114
nanako45a <nsz.rnmk120@gmail.com>
2024-01-27 14:06:21 +0900

    carrierwaveの設定を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,16 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'                        
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'findlibrary' # S3バケットの名前                        
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1' # バケットのリージョン
+      region: 'ap-northeast-1',# バケットのリージョン
+      path_style: true
     }
-    config.fog_directory  = 'findlibrary' # S3バケットの名前
-  end
-  
+end

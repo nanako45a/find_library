582416f03d8ba8e69b99f9f2039c2bfc3cedbae4
nanako45a <nsz.rnmk120@gmail.com>
2024-03-09 23:35:26 +0900

    🔨Fix:bookmarkのajax化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,29 @@
 class BookmarksController < ApplicationController
-  # ログインしていることを確認
   before_action :require_login
-
-  # お気に入り登録、解除実行前にset_libraryメソッドを実行するように指定
   before_action :set_library
 
   # お気に入り登録
   def create
     bookmark = current_user.bookmarks.build(library: @library)
     if bookmark.save
-      # 登録成功でライブラリページにリダイレクトし、成功メッセージ表示
-      redirect_to @library, notice: 'お気に入りに登録しました'
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @library, notice: 'お気に入りに登録しました' }
+      end
     else
-      # 登録失敗でライブラリページにリダイレクトし、エラーメッセージ表示
-      redirect_to @library, alert: 'お気に入りの登録に失敗しました'
+      redirect_to @library, alert:'お気に入りの登録に失敗しました'
     end
   end
 
   # お気に入り解除
   def destroy
-    # ユーザーがお気に入り解除に指定した、登録済みの図書館IDを検索し削除
     bookmark = current_user.bookmarks.find_by(library_id: @library.id)
     if bookmark.destroy
-      # 削除成功でライブラリページにリダイレクトし、成功メッセージ表示
-      redirect_to @library, notice: 'お気に入りを解除しました'
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @library, notice: 'お気に入りを解除しました' }
+      end
     else
-      # 削除成功でライブラリページにリダイレクトし、エラーメッセージ表示
       redirect_to @library, alert: 'お気に入りの解除に失敗しました'
     end
   end

d398452afdccf70d1d140042a73bf448cfb07a06
nanako45a <nsz.rnmk120@gmail.com>
2024-03-12 19:29:38 +0900

    🔨🔨Fix:アソシエーションを利用せず定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,11 @@ class CommentsController < ApplicationController
   before_action :require_login
 
   def create
-    # コメントを追加する図書館を特定
+    # コメントする図書館を特定
     @library = Library.find(params[:library_id])
-    # 特定の図書館に新しいコメントを構築し、現在ログインしているユーザーをコメントユーザーとして設定
-    @comment = @library.comments.build(comment_params)
+    # 新しいコメントを直接作成し、パラメーターからlibrary_idを取得して設定
+    @comment = Comment.new(comment_params)
     @comment.user = current_user
-    # コメント保存の条件分岐
     if @comment.save
       redirect_to @library, notice: 'コメントを追加しました。'
     else
@@ -17,12 +16,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    # 削除するコメントを特定
+    # 削除するコメントと属する図書館を特定
     @comment = Comment.find(params[:id])
-    # コメントが属する図書館を特定
     @library = @comment.library
-    # 現在ログイン中のユーザーによって作成された場合のみコメントを削除
-    # コメント削除の条件分岐
     if @comment.user == current_user
       @comment.destroy
       redirect_to @library, notice: 'コメントを削除しました。'
@@ -36,6 +32,6 @@ class CommentsController < ApplicationController
   # Strong Parameters
   # フォームから送信されたデータのうち、許可されたパラメータのみ使用可能にする
   def comment_params
-    params.require(:comment).permit(:comments_body, :comments_seats_number, :comments_pc_available, :comments_wifi_available, :comments_power_available)
+    params.require(:comment).permit(:comments_body, :comments_seats_number, :comments_pc_available, :comments_wifi_available, :comments_power_available, :library_id)
   end
 end
